7e50cde6d322e8ceb9ca8d962f2261c5d44fb101
Marsel <marselbairam@gmail.com>
2021-09-05 12:44:40 +0300

    make article create endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { TagModule } from '@app/tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from '@app/ormconfig';
 import { UserModule } from '@app/user/user.module';
+import { ArticleModule } from '@app/article/article.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TagModule,
+    UserModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/article/article.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { UserEntity } from '@app/user/user.entity';
+import { CreateArticleDto } from '@app/article/dto/create-article.dto';
+import { ArticleEntity } from '@app/article/article.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ArticleResponseInterface } from '@app/article/types/article-response.interface';
+import slugify from 'slugify';
+
+@Injectable()
+export class ArticleService {
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    article.slug = this.getSlug(createArticleDto.title);
+
+    article.author = currentUser;
+
+    return await this.articleRepository.save(article);
+  }
+
+  buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+    return { article };
+  }
+
+  private getSlug(title: string): string {
+    return (
+      slugify(title, {
+        lower: true,
+      }) +
+      '-' +
+      ((Math.random() * Math.pow(36, 6)) | 0).toString(36)
+    );
+  }
+}
