3bb267b845ea784cbf8c15d55d649bb896d5476c
Marsel <marselbairam@gmail.com>
2021-10-03 21:46:31 +0300

    add throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,14 @@ import ormconfig from '../ormconfig';
 import { UserModule } from '@app/user/user.module';
 import { ArticleModule } from '@app/article/article.module';
 import { ProfileModule } from '@app/profile/profile.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
     TypeOrmModule.forRoot(ormconfig),
     TagModule,
     UserModule,
